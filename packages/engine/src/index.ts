e9f4409fb389454ffe6042c473a7d882f16506be
dunay2 <adream0@gmail.com>
2026-02-20 20:12:28 +0000

    index

--- packages/engine/src/index.ts
+++ packages/engine/src/index.ts
@@ -2,6 +2,7 @@ export * from './contracts/IWorkflowEngine.v1_1_1.js';
 export * from './contracts/types.js';
 export * from './contracts/runEvents.js';
 export * from './contracts/executionPlan.js';
+export * from './contracts/errors.js';
 
 export * from './core/WorkflowEngine.js';
 export * from './core/SnapshotProjector.js';
@@ -11,16 +12,20 @@ export * from './state/IRunStateStore.js';
 export * from './state/InMemoryTxStore.js';
 
 export * from './outbox/types.js';
+export * from './outbox/IOutboxRateLimiter.js';
+export * from './outbox/TokenBucketRateLimiter.js';
 export * from './outbox/OutboxWorker.js';
 export * from './outbox/InMemoryEventBus.js';
 
 export * from './utils/clock.js';
+export * from './metrics/IMetricsCollector.js';
 
 export * from './security/authorizer.js';
 export * from './security/AuthorizationError.js';
 export * from './security/planRefPolicy.js';
 export * from './security/planIntegrity.js';
 
+export * from './adapters/IPlanFetcher.js';
 export * from './adapters/IProviderAdapter.js';
 export * from './adapters/mock/MockAdapter.js';
 export * from './adapters/temporal/TemporalAdapterStub.js';
