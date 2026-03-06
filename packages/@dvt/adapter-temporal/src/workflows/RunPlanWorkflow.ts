cb251f73dfb56874ac20e3fd852b20f33bee528e
dunay2 <adream0@gmail.com>
2026-03-06 15:26:22 +0000

    chore(adapter-temporal): remove obsolete eslint override

--- packages/@dvt/adapter-temporal/src/workflows/RunPlanWorkflow.ts
+++ packages/@dvt/adapter-temporal/src/workflows/RunPlanWorkflow.ts
@@ -21,7 +21,6 @@
  *  - Zero `process.env`
  *  - Zero Node.js / DOM APIs
  */
-// eslint-disable-next-line import/no-unresolved -- changed-file lint runs before workspace build.
 import { planExecutionLayers } from '@dvt/plan-interpreter';
 import {
   ApplicationFailure,
