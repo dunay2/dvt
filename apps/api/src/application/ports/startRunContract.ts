7ac023bb89f9734505aff459142775d0c6867c93
dunay2 <adream0@gmail.com>
2026-04-01 19:37:53 +0100

    fix(api): Format startRun adapter-registry files for pre-push

--- apps/api/src/application/ports/startRunContract.ts
+++ apps/api/src/application/ports/startRunContract.ts
@@ -1,6 +1,4 @@
-export {
-  type IStartRunTargetAdapterRegistry,
-} from './IStartRunTargetAdapterRegistry.js';
+export { type IStartRunTargetAdapterRegistry } from './IStartRunTargetAdapterRegistry.js';
 export {
   START_RUN_TARGET_ADAPTER,
   type StartRunTargetAdapter,
