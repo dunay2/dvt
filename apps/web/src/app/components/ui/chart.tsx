375f706c397bca986213fa8d650317202f55f539
dunay2 <adream0@gmail.com>
2026-04-08 12:20:14 +0100

    style(web): Format chart tooltip and legend typing

--- apps/web/src/app/components/ui/chart.tsx
+++ apps/web/src/app/components/ui/chart.tsx
@@ -240,11 +240,11 @@ function ChartLegendContent({
   verticalAlign = 'bottom',
   nameKey,
 }: React.ComponentProps<'div'> & {
-    payload?: ReadonlyArray<LegendPayload>;
-    verticalAlign?: RechartsPrimitive.LegendProps['verticalAlign'];
-    hideIcon?: boolean;
-    nameKey?: string;
-  }) {
+  payload?: ReadonlyArray<LegendPayload>;
+  verticalAlign?: RechartsPrimitive.LegendProps['verticalAlign'];
+  hideIcon?: boolean;
+  nameKey?: string;
+}) {
   const { config } = useChart();
 
   if (!payload?.length) {
