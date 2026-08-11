a53b9d553fe3e704181ca21d62127d11b269f6db
dunay2 <adream0@gmail.com>
2026-08-11 02:51:26 +0100

    fix(api): Require explicit route operational dependencies

---
+++ apps/api/test/entrypoints/http/routeDependencyContracts.typecheck.ts
@@ -0,0 +1,41 @@
+import type { IObservability } from '@dvt/observability';
+
+import type { IStartRunTargetAdapterRegistry } from '../../../src/application/ports/IStartRunTargetAdapterRegistry.js';
+import { startRunRoute } from '../../../src/entrypoints/http/startRunRoute.js';
+import { workspaceContextRoute } from '../../../src/entrypoints/http/workspaceContextRoute.js';
+
+type AssertFalse<Value extends false> = Value;
+type AssertTrue<Value extends true> = Value;
+type IsOptional<ObjectType, Key extends keyof ObjectType> =
+  {} extends Pick<ObjectType, Key> ? true : false;
+
+type StartRunRouteParameters = Parameters<typeof startRunRoute>;
+type StartRunRouteDependencies = NonNullable<StartRunRouteParameters[3]>;
+type StartRunRouteWithoutDependencies = (
+  request: StartRunRouteParameters[0],
+  reply: StartRunRouteParameters[1],
+  facade: StartRunRouteParameters[2]
+) => ReturnType<typeof startRunRoute>;
+
+export type StartRunRouteRequiresDependenciesArgument = AssertFalse<
+  typeof startRunRoute extends StartRunRouteWithoutDependencies ? true : false
+>;
+export type StartRunRouteRequiresAdapterRegistry = AssertFalse<
+  IsOptional<StartRunRouteDependencies, 'adapterRegistry'>
+>;
+export type StartRunRouteRequiresObservability = AssertFalse<
+  IsOptional<StartRunRouteDependencies, 'observability'>
+>;
+export type StartRunRouteDependencyTypesAreOwned = AssertTrue<
+  StartRunRouteDependencies['adapterRegistry'] extends IStartRunTargetAdapterRegistry
+    ? StartRunRouteDependencies['observability'] extends IObservability
+      ? true
+      : false
+    : false
+>;
+
+type WorkspaceContextRouteDependencies = Parameters<typeof workspaceContextRoute>[2];
+
+export type WorkspaceContextRouteRequiresAdapterRegistry = AssertFalse<
+  IsOptional<WorkspaceContextRouteDependencies, 'adapterRegistry'>
+>;
